c3edcb49bf2f1f8e3745c388b4683701a7d3839b
Anastasiya <dunas1@yandex.ru>
2025-11-02 22:24:43 +0800

    убрала неисп импорт

--- stats-client/src/main/java/ru/practicum/StatsClient.java
+++ stats-client/src/main/java/ru/practicum/StatsClient.java
@@ -11,8 +11,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
+
 
 @RequiredArgsConstructor
 public class StatsClient {
