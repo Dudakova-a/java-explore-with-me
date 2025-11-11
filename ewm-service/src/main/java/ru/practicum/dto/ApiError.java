cdeaef46d882c6ccb89ce090bf2c731f541adc6a
Anastasiya <dunas1@yandex.ru>
2025-11-11 22:05:58 +0800

    выполнен этап 2 попытка загрузить

--- ewm-service/src/main/java/ru/practicum/dto/ApiError.java
+++ ewm-service/src/main/java/ru/practicum/dto/ApiError.java
@@ -18,7 +18,7 @@ public class ApiError {
     private String message;
     private String reason;
     private String status;
-    
+
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
